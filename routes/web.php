d6f10b0a2cc48d298080739bd4a41b460a781016
fouadsalkini <fouad@sync.com.lb>
2024-05-27 16:24:01 +0300

    added debugbar

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\DefaultController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DefaultController::class, 'pdf']);
+Route::get('/', function(){
+    $text = 'hello world';
+    return view('welcome', compact('text'));
+});
+Route::get('/pdf', [DefaultController::class, 'pdf']);
